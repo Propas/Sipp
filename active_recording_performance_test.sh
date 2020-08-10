4c4a57aa172bf5563a4bc7fe2db32f32b8fa73eb
Michal Propilek <michal.propilek@zoomint.com>
2020-08-10 20:03:01 +0200

    refactoring

---
+++ active_recording_performance_test.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+../../sipp/sipp 10.17.7.16:5060 -sf Scenarios/cisco_active_recording_nearend.xml -s 599919 -m 1
+#../../sipp/sipp 10.17.7.16:5060 -sf Scenarios/cisco_active_recording_farend.xml -s 599919 -m 1
