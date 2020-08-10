7733024a4c6c6277f91e443ea727c48487ddbaea
Michal Propilek <michal.propilek@zoomint.com>
2020-08-10 16:10:55 +0200

    Cisco active recording scenario created

---
+++ Scenarios/active_recording_performance_test.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+c:/Users/michal.propilek/sipp/sipp 10.17.7.16:5060 -sf cisco_active_recording_nearend.xml -s 599919 -m 1
+c:/Users/michal.propilek/sipp/sipp 10.17.7.16:5060 -sf cisco_active_recording_farend.xml -s 599919 -m 1
