289ab1b606fb2f08c42cd367098c05bf7e4e275d
Michal Propilek <michal.propilek@zoomint.com>
2020-08-11 16:32:42 +0200

    added parameter for number of calls

--- active_recording_performance_test.sh
+++ active_recording_performance_test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-sudo ~/sipp/sipp 10.17.7.16:5060 -sf ~/projects/Sipp/Scenarios/cisco_active_recording_nearend.xml -s 599919 -r 1 -rp 2000 -m 5 -bg
-sudo ~/sipp/sipp 10.17.7.16:5060 -sf ~/projects/Sipp/Scenarios/cisco_active_recording_farend.xml -s 599919 -r 1 -rp 2000 -m 5 -bg
+sudo ~/sipp/sipp 10.17.7.16:5060 -sf ~/projects/Sipp/Scenarios/cisco_active_recording_nearend.xml -s 599919 -r 1 -rp 2000 -m $1 -bg
+sudo ~/sipp/sipp 10.17.7.16:5060 -sf ~/projects/Sipp/Scenarios/cisco_active_recording_farend.xml -s 599919 -r 1 -rp 2000 -m $1 -bg
 
 
 
