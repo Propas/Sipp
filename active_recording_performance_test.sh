b0d28fefe3f832caa3fc31fd38aeb45e9ba50d17
Michal Propilek <michal.propilek@gmail.com>
2020-08-11 13:03:49 +0200

    Sipp is running on the background

--- active_recording_performance_test.sh
+++ active_recording_performance_test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-../../sipp/sipp 10.17.7.16:5060 -sf Scenarios/cisco_active_recording_nearend.xml -s 599919 -m 1
-#../../sipp/sipp 10.17.7.16:5060 -sf Scenarios/cisco_active_recording_farend.xml -s 599919 -m 1
+../../sipp/sipp 10.17.7.16:5060 -sf Scenarios/cisco_active_recording_nearend.xml -s 599919 -m 1 -bg
+../../sipp/sipp 10.17.7.16:5060 -sf Scenarios/cisco_active_recording_farend.xml -s 599919 -m 1 -bg
