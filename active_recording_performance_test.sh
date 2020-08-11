540863761e7d266f3fb6cf4948ea326271208bd4
Michal Propilek <michal.propilek@zoomint.com>
2020-08-11 15:36:03 +0200

    loop for new calls

--- active_recording_performance_test.sh
+++ active_recording_performance_test.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
-sudo ~/sipp/sipp 10.17.7.16:5060 -sf ~/projects/Sipp/Scenarios/cisco_active_recording_nearend.xml -s 599919 -m 1 -bg
-sudo ~/sipp/sipp 10.17.7.16:5060 -sf ~/projects/Sipp/Scenarios/cisco_active_recording_farend.xml -s 599919 -m 1 -bg
+LOOP_LIMIT=$1
+
+for (( VAR = 0; VAR <= ${LOOP_LIMIT}; ++VAR )); do
+    sudo ~/sipp/sipp 10.17.7.16:5060 -sf ~/projects/Sipp/Scenarios/cisco_active_recording_nearend.xml -s ${LOOP_LIMIT} -m 1 -bg
+    sudo ~/sipp/sipp 10.17.7.16:5060 -sf ~/projects/Sipp/Scenarios/cisco_active_recording_farend.xml -s ${LOOP_LIMIT} -m 1 -bg
+    sleep 1000;
+done
+
+
