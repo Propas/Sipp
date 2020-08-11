a893c2e15b0404151db4702a19e0cdd33bc087e4
Michal Propilek <michal.propilek@gmail.com>
2020-08-11 14:45:01 +0200

    enhanced script

--- active_recording_performance_test.sh
+++ active_recording_performance_test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-../../sipp/sipp 10.17.7.16:5060 -sf Scenarios/cisco_active_recording_nearend.xml -s 599919 -m 1 -bg
-../../sipp/sipp 10.17.7.16:5060 -sf Scenarios/cisco_active_recording_farend.xml -s 599919 -m 1 -bg
+sudo ~/sipp/sipp 10.17.7.16:5060 -sf ~/projects/Sipp/Scenarios/cisco_active_recording_nearend.xml -s 599919 -m 1 -bg
+sudo ~/sipp/sipp 10.17.7.16:5060 -sf ~/projects/Sipp/Scenarios/cisco_active_recording_farend.xml -s 599919 -m 1 -bg
