31a7a455363526a81ed321e6c2f7994276ffdcaf
Michal Propilek <michal.propilek@zoomint.com>
2020-08-12 10:05:03 +0200

    prepare for test parameters

--- active_recording_performance_test.sh
+++ active_recording_performance_test.sh
@@ -8,20 +8,22 @@ CALLS_PER_SECONDS=10
 RATE_PERIOD=1
 MAX_CALL_COUNT=1000
 
-while getopts "h:" opt; do
+usage() { error "Usage: $0 -s <SERVICE_NUMBER> -c <CALLS_PER_SECONDS> -p [-v <RATE_PERIOD>] [-m <MAX_CALL_COUNT>] [-h <HELP>]"; }
+
+while getopts ":scpmh" opt; do
 	case ${opt} in
-#	  s)
-#		  SERVICE_NUMBER=${OPTARG}
-#		  ;;
-#	  r)
-#		  CALLS_PER_SECONDS=${OPTARG}
-#		  ;;
-#	  rp)
-#		  RATE_PERIOD=${OPTARG}
-#		  ;;
-#	  m)
-#		  MAX_CALL_COUNT=${OPTARG}
-#		  ;;
+	  s)
+		  SERVICE_NUMBER=${OPTARG}
+		  ;;
+	  c)
+		  CALLS_PER_SECONDS=${OPTARG}
+		  ;;
+	  p)
+		  RATE_PERIOD=${OPTARG}
+		  ;;
+	  m)
+		  MAX_CALL_COUNT=${OPTARG}
+		  ;;
 	  h)
 		  usage
 		  exit
@@ -37,8 +39,6 @@ while getopts "h:" opt; do
       ;;
     esac
 done
-
-usage() { error "Usage: $0 -s <SERVICE_NUMBER> -r <CALLS_PER_SECONDS> -rp [-v <RATE_PERIOD>] [-m <MAX_CALL_COUNT>] [-h <HELP>]"; }
 
 sudo ~/sipp/sipp 10.17.7.16:5060 -sf ~/projects/Sipp/Scenarios/cisco_active_recording_nearend.xml -s ${SERVICE_NUMBER} -r ${CALLS_PER_SECONDS} -rp ${RATE_PERIOD} -m ${MAX_CALL_COUNT} -bg
 sudo ~/sipp/sipp 10.17.7.16:5060 -sf ~/projects/Sipp/Scenarios/cisco_active_recording_farend.xml -s ${SERVICE_NUMBER} -r ${CALLS_PER_SECONDS} -rp ${RATE_PERIOD} -m ${MAX_CALL_COUNT} -bg
