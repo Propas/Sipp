77eaacc8e536a1121efd5156f06596907bc14870
Michal Propilek <michal.propilek@zoomint.com>
2020-08-12 12:57:28 +0200

    add quotation marks

--- active_recording_performance_test.sh
+++ active_recording_performance_test.sh
@@ -10,23 +10,23 @@ MAX_CALL_COUNT=1000
 usage()
 {
   echo "Usage: $0: [-s|c|p|m] - Default -s 599919 -c 10 -p 1 -m 1000. 10 calls every 1 sec up to 1000 calls"
-  echo "-s  service number is paste to 'to header' in sip"
-  echo "-c  calls per second"
-  echo "-p  rate period"
-  echo "-m  max call count"
-  echo "-h  help"
+  echo "  -s  service number is paste to 'to header' in sip"
+  echo "  -c  calls per second"
+  echo "  -p  rate period"
+  echo "  -m  max call count"
+  echo "  -h  help"
 }
 
 while getopts ":scpmh" opt; do
 	case ${opt} in
 	  s)
-		  SERVICE_NUMBER=${OPTARG};;
+		  SERVICE_NUMBER="${OPTARG}";;
 	  c)
-		  CALLS_PER_SECONDS=${OPTARG};;
+		  CALLS_PER_SECONDS="${OPTARG}";;
 	  p)
-		  RATE_PERIOD=${OPTARG};;
+		  RATE_PERIOD="${OPTARG}";;
 	  m)
-		  MAX_CALL_COUNT=${OPTARG};;
+		  MAX_CALL_COUNT="${OPTARG}";;
 	  h)
 		  usage
 		  exit;;
@@ -40,8 +40,8 @@ while getopts ":scpmh" opt; do
     esac
 done
 
-sudo ~/sipp/sipp 10.17.7.16:5060 -sf ~/projects/Sipp/Scenarios/cisco_active_recording_nearend.xml -s ${SERVICE_NUMBER} -r ${CALLS_PER_SECONDS} -rp ${RATE_PERIOD} -m ${MAX_CALL_COUNT} -bg
-sudo ~/sipp/sipp 10.17.7.16:5060 -sf ~/projects/Sipp/Scenarios/cisco_active_recording_farend.xml -s ${SERVICE_NUMBER} -r ${CALLS_PER_SECONDS} -rp ${RATE_PERIOD} -m ${MAX_CALL_COUNT} -bg
+sudo ~/sipp/sipp 10.17.7.16:5060 -sf ~/projects/Sipp/Scenarios/cisco_active_recording_nearend.xml -s "${SERVICE_NUMBER}" -r "${CALLS_PER_SECONDS}" -rp "${RATE_PERIOD}" -m "${MAX_CALL_COUNT}" -bg
+sudo ~/sipp/sipp 10.17.7.16:5060 -sf ~/projects/Sipp/Scenarios/cisco_active_recording_farend.xml -s "${SERVICE_NUMBER}" -r "${CALLS_PER_SECONDS}" -rp "${RATE_PERIOD}" -m "${MAX_CALL_COUNT}" -bg
 
 
 
