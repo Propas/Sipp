5a1074f5d7cd8154a8ee1d0ff97a5d5296677979
Michal Propilek <michal.propilek@zoomint.com>
2020-08-12 09:15:30 +0200

    prepare for test parameters

--- active_recording_performance_test.sh
+++ active_recording_performance_test.sh
@@ -1,35 +1,44 @@
 #!/usr/bin/env bash
 
+set -e
+set -u
 
-usage() { error "Usage: $0 -s <SERVICE_NUMBER> -r <CALLS_PER_SECONDS> -rp [-v <RATE_PERIOD>] [-m <MAX_CALL_COUNT>] [-h <HELP>]"; }
+SERVICE_NUMBER=599919
+CALLS_PER_SECONDS=10
+RATE_PERIOD=1
+MAX_CALL_COUNT=1000
 
-while getopts "s:r:rp:m:h:" opt; do
+while getopts "h:" opt; do
 	case ${opt} in
-	s)
-		SERVICE_NUMBER=${OPTARG};;
-	r)
-		CALLS_PER_SECONDS=${OPTARG};;
-	rp)
-		RATE_PERIOD=${OPTARG};;
-	m)
-		MAX_CALL_COUNT=${OPTARG};;
-	h)
-		usage
-		exit;;
-	\?)
-    usage
-    exit 1;;
-  :)
-    error "Option -$OPTARG requires an argument." >&2
-    usage
-    exit 1;;
-  esac
+#	  s)
+#		  SERVICE_NUMBER=${OPTARG}
+#		  ;;
+#	  r)
+#		  CALLS_PER_SECONDS=${OPTARG}
+#		  ;;
+#	  rp)
+#		  RATE_PERIOD=${OPTARG}
+#		  ;;
+#	  m)
+#		  MAX_CALL_COUNT=${OPTARG}
+#		  ;;
+	  h)
+		  usage
+		  exit
+		  ;;
+	  \?)
+      usage
+      exit 1
+      ;;
+    :)
+      error "Option -${OPTARG} requires an argument." >&2
+      usage
+      exit 1
+      ;;
+    esac
 done
 
-if [[ -z ${SERVICE_NUMBER} ]]; then
-    SERVICE_NUMBER=599919
-fi
-
+usage() { error "Usage: $0 -s <SERVICE_NUMBER> -r <CALLS_PER_SECONDS> -rp [-v <RATE_PERIOD>] [-m <MAX_CALL_COUNT>] [-h <HELP>]"; }
 
 sudo ~/sipp/sipp 10.17.7.16:5060 -sf ~/projects/Sipp/Scenarios/cisco_active_recording_nearend.xml -s ${SERVICE_NUMBER} -r ${CALLS_PER_SECONDS} -rp ${RATE_PERIOD} -m ${MAX_CALL_COUNT} -bg
 sudo ~/sipp/sipp 10.17.7.16:5060 -sf ~/projects/Sipp/Scenarios/cisco_active_recording_farend.xml -s ${SERVICE_NUMBER} -r ${CALLS_PER_SECONDS} -rp ${RATE_PERIOD} -m ${MAX_CALL_COUNT} -bg
